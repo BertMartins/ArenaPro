db048a260b29bc25de6914cfbcb122ae3b940dd9
Heberte <heberteuallan1@gmail.com>
2025-12-06 21:27:59 -0300

    removendo pasta e arquivo que não é mais usado

--- src/app/(dashboard)/layout.tsx
+++
@@ -1,8 +0,0 @@
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen pb-20"> 
-      {/* Conteúdo */}
-      <main className="p-4">{children}</main>
-    </div>
-  );
-}
